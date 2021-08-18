88344f6c53060b603f0bd66cea1ff81213cbc74d
近藤米功 <foad5969@gmail.com>
2021-08-18 10:19:09 +0900

    Initial Commit

---
+++ tictok_mikever/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tictok_mikever
+//
+//  Created by 近藤米功 on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
